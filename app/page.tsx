8b61ead588393caef82bbdef6df7449ffbb97462
lesz87 <lesliezacharski@gmail.com>
2024-07-22 12:03:46 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -87,7 +87,7 @@ export default function Home() {
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
+            Explore starter templates for Next.js and the other Vercel products 12345623
           </p>
         </a>
 
